f46fed23ac585330946f51b1638c0dae9bbece1e
mateusdreher <mateusdreher88@gmail.com>
2020-07-18 04:17:09 -0300

    Changed request

--- js/provider.js
+++ js/provider.js
@@ -5,7 +5,6 @@ function request(method, endpoint, body){
     let url = `${path}/${endpoint}`;
 
     xhr.open(method.toUpperCase(), url, true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             if (xhr.status = 200){
